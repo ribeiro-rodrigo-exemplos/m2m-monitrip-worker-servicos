63afe14581f649c0adcd390e628b5dde36712538
Rodrigo Ribeiro <rodrigo.rs1989@gmail.com>
2017-04-18 11:00:33 -0300

    colocando caminho do csv no arquivo de configuracoes

--- src/main/java/br/com/m2msolutions/monitriip/workerservicos/routes/PontosRoute.java
+++ src/main/java/br/com/m2msolutions/monitriip/workerservicos/routes/PontosRoute.java
@@ -3,7 +3,11 @@ package br.com.m2msolutions.monitriip.workerservicos.routes;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.csv.CsvDataFormat;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
 /**
  * Created by Rodrigo Ribeiro on 10/04/17.
  */
@@ -13,12 +17,15 @@ public class PontosRoute extends RouteBuilder{
     @Autowired
     private CsvDataFormat csvFormat;
 
+    @Value("${pontos-path-file}")
+    private String pontosPathFile;
+
     @Override
     public void configure() throws Exception {
 
         onException(Exception.class).process(e -> {}).handled(true);
 
-        from("file:src/main/resources/files?noop=true").
+        from(String.format("file:%s?noop=true",pontosPathFile)).
             log("Carregando pontos...").
             unmarshal(csvFormat).
                 split().
